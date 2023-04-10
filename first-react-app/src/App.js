b9e485ca333d73f86d076552c1210aaa75f8628d
RoyMidence <Roy.AMidence@Gmail.com>
2023-04-10 16:07:34 -0400

    WOWY REACT IS ACTUALLY VERY COOL, GONNA STUDY

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; // Wow, this is pretty cool, is this normal
 import './App.css';
+
+const mySelf = {
+  name: "Roy Midence",
+  date: "4/10/2023"
+};
 
 function MyButton() {
   return (
@@ -9,13 +14,17 @@ function MyButton() {
 
 function App() {
   return (
+    // Amywhere I see className is a thing in the App.CSS file, veyr neat
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           We're Grooving (Testing things)
         </p>
-        <MyButton />
+        <MyButton /> {/* This is also very cool, have to comment like this to appease JSX lords*/}
+        <p>
+          Made by: <b>{mySelf.name}</b> on {mySelf.date}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
